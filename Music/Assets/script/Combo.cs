a4ca2b54b6f50d29ecb2373693ce12a371f89d83
yuzu <yuzukaw@yahoo.co.jp>
2017-08-07 19:28:18 +0900

    コンボ(仮)作成

---
+++ Music/Assets/script/Combo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combo : MonoBehaviour {
+
+    TextMesh tex;
+    int combo;
+
+    // Use this for initialization
+    void Start () {
+        tex = GetComponent<TextMesh>();
+        combo = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddCombo() {
+        combo += 1;
+        tex.text = "コンボ：" + combo;
+    }
+
+    public void ComboReset() {
+        combo = 0;
+        tex.text = "コンボ：" + combo;
+    }
+}
--- Music/Assets/script/Note.cs
+++ Music/Assets/script/Note.cs
@@ -7,12 +7,14 @@ public class Note : MonoBehaviour {
     ChangeText changeText;
 
     Score score;
+    Combo combo;
     const float speed = 1.0f;
     Vector3 scale;
 
     // Use this for initialization
     void Start () {
         score = GameObject.Find("Score").GetComponent<Score>();
+        combo = GameObject.Find("Combo").GetComponent<Combo>();
         transform.localScale = new Vector3(4, 4, 1);
         scale = transform.localScale;
     }
@@ -30,22 +32,26 @@ public class Note : MonoBehaviour {
             //excellent
             changeText.WriteTex("excellent");
             score.AddScore(100);
+            combo.AddCombo();
         }
         else if (transform.localScale.x <= 1.6f && transform.localScale.x > 1.2f ||
                  transform.localScale.x < 0.8f && transform.localScale.x >= 0.4f) {
             //great
             changeText.WriteTex("great");
             score.AddScore(50);
+            combo.AddCombo();
         }
         else if (transform.localScale.x <= 2.0f && transform.localScale.x > 1.6f ||
                  transform.localScale.x < 0.4f && transform.localScale.x > 0.0f){
             //good
             changeText.WriteTex("good");
             score.AddScore(10);
+            combo.AddCombo();
         }
         else {
             //bad
             changeText.WriteTex("bad");
+            combo.ComboReset();
         }
         Destroy(gameObject);
     }
